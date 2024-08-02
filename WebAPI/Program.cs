f8982b64b0e5f6235d8d5b2a85ceae2c1d04fd9d
Umut <57362066+umutakpinar@users.noreply.github.com>
2024-08-02 16:16:42 +0300

    feat: connection string added to IoC & oracle driver added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<RepositoryContext>(
+    optionsBuilder => optionsBuilder.UseOracle(builder.Configuration.GetConnectionString("OracleSQLConnection"))
+    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,7 +21,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}  
 
 app.UseHttpsRedirection();
 
